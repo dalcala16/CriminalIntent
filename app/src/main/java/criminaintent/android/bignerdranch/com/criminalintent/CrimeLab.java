9158fb8f1a033ba0c6de6696e20ae098c08234b1
dalcala16 <dalcala16@yahoo.com>
2015-11-04 15:15:38 -0800

    Lab 3-2

---
+++ app/src/main/java/criminaintent/android/bignerdranch/com/criminalintent/CrimeLab.java
@@ -0,0 +1,46 @@
+package criminaintent.android.bignerdranch.com.criminalintent;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+/**
+ * Created by student on 11/3/2015.
+ */
+public class CrimeLab {
+    private ArrayList<Crime> mCrimes;
+
+    private static CrimeLab sCrimeLab;
+    private Context mAppContext;
+
+    private CrimeLab(Context appContext) {
+        mAppContext = appContext;
+        mCrimes = new ArrayList<Crime>();
+        for (int i = 0; i < 100; i++) {
+            Crime c = new Crime();
+            c.setTitle("Crime #" + i);
+            c.setSolved(i % 2 == 0);
+            mCrimes.add(c);
+        }
+    }
+
+    public ArrayList<Crime> getCrimes() {
+        return mCrimes;
+    }
+
+    public static CrimeLab get(Context c) {
+        if (sCrimeLab == null) {
+            sCrimeLab = new CrimeLab(c.getApplicationContext());
+        }
+        return sCrimeLab;
+    }
+
+    public Crime getCrime(UUID id) {
+        for (Crime c : mCrimes) {
+            if (c.getId().equals(id))
+                return c;
+        }
+        return null;
+    }
+}
